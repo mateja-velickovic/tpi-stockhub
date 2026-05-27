1d123fa016166b120bb96786b497cb6d25040494
Mateja Velickovic <mateja.velickovic@eduvaud.ch>
2026-05-27 08:41:57 +0200

    fix(vite): Ajustement de la configuration de vite, ajout d'une config de staging

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,26 +2,36 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 
-export default defineConfig({
-  plugins: [vue()],
-  envDir: resolve(__dirname, '..'),
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
-    },
-  },
-  preview: {
-    port: parseInt(process.env.VITE_DEV_PORT || '5173'),
-    proxy: {
-      '/api': {
-        target: process.env.VITE_API_PROXY_TARGET || 'http://localhost:3000',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const isStaging = process.env.VITE_ENV === 'staging';
+
+  return {
+    plugins: [vue()],
+    envDir: resolve(__dirname, '..'),
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src'),
       },
     },
-  },
-  server: {
-    host: true,
-    port: parseInt(process.env.VITE_DEV_PORT || '5173'),
-    allowedHosts: ["stockhub.pm2etml.ch"],
-  },
+    preview: {
+      port: parseInt(process.env.VITE_DEV_PORT || '5173'),
+      proxy: {
+        '/api': {
+          target: process.env.VITE_API_PROXY_TARGET || 'http://localhost:3000',
+          changeOrigin: true,
+        },
+      },
+    },
+    server: isStaging
+      ? {
+          host: true,
+          port: parseInt(process.env.VITE_DEV_PORT), 
+          allowedHosts: ["staging.stockhub.pm2etml.ch"],
+        }
+      : {
+          host: true,
+          port: parseInt(process.env.VITE_DEV_PORT),
+          allowedHosts: ["stockhub.pm2etml.ch"],
+        },
+  };
 });
