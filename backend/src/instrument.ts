80616134c37dd848770d254ba719f611d16fc2fe
Mateja Velickovic <mateja.velickovic@eduvaud.ch>
2026-05-29 09:14:15 +0200

    fix(sentry): Modification de l'import de Sentry

--- backend/src/instrument.ts
+++ backend/src/instrument.ts
@@ -1,4 +1,4 @@
-import Sentry from "@sentry/node";
+import * as Sentry from "@sentry/node";
 
 Sentry.init({
   dsn: "https://d1271ded720458c379ea325575e21c43@o4511302587187200.ingest.de.sentry.io/4511471770206288",
