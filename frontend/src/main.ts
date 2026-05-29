bea9ec0f6c9b4710a810d7ad0b9a29c436d0a5c2
Mateja Velickovic <mateja.velickovic@eduvaud.ch>
2026-05-29 08:49:58 +0200

    feat(app): Initialisation de Sentry dans le frontend

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,10 +3,19 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 import './assets/main.css';
+import * as Sentry from "@sentry/vue";
 
 const app = createApp(App);
 
+Sentry.init({
+  app,
+  dsn: "https://22e1aef9c2231698182a0541ef8069e4@o4511302587187200.ingest.de.sentry.io/4511471842426960",
+  sendDefaultPii: true
+});
+
 app.use(createPinia());
 app.use(router);
+
+
 
 app.mount('#app');
