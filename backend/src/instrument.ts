dba11537fcd26e394560dbfdf014083f8cb3bc7f
mateja-velickovic <mateja.velickovic@eduvaud.ch>
2026-05-29 08:38:40 +0200

    feat(sentry): Ajout du fichier d'init pour Sentry

---
+++ backend/src/instrument.ts
@@ -0,0 +1,8 @@
+import Sentry from "@sentry/node";
+
+Sentry.init({
+  dsn: "https://d1271ded720458c379ea325575e21c43@o4511302587187200.ingest.de.sentry.io/4511471770206288",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+});
